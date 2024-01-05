e987b7d67506368bf30c2aeef9cc11f81fc4aaca
Josh Rittenberry <141363044+JoshRittenberry@users.noreply.github.com>
2024-01-05 10:45:24 -0600

    Get Appointment by ID Endpoint Completed

--- Program.cs
+++ Program.cs
@@ -85,6 +85,61 @@ app.MapGet("/api/appointments", (HillarysHairSalonDbContext db) =>
 });
 
 // 2. Endpoint to get an Appointment by Id
+app.MapGet("/api/appointments/{id}", (HillarysHairSalonDbContext db, int id) =>
+{
+    var appointment = db.Appointments
+        .Include(a => a.Stylist)
+        .Include(a => a.Customer)
+        .Include(a => a.AppointmentServices)
+            .ThenInclude(aserv => aserv.Service)
+        .SingleOrDefault(a => a.Id == id);
+
+    return Results.Ok(new AppointmentDTO
+    {
+        Id = appointment.Id,
+        StylistId = appointment.StylistId,
+        Stylist = new StylistDTO
+        {
+            Id = appointment.Stylist.Id,
+            FirstName = appointment.Stylist.FirstName,
+            LastName = appointment.Stylist.LastName,
+            PhoneNumber = appointment.Stylist.PhoneNumber,
+            Email = appointment.Stylist.Email,
+            Password = appointment.Stylist.Password,
+            StartDate = appointment.Stylist.StartDate,
+            EndDate = appointment.Stylist.EndDate,
+            IsActive = appointment.Stylist.IsActive,
+            IsAdmin = appointment.Stylist.IsAdmin,
+            Appointments = null
+        },
+        CustomerId = appointment.CustomerId,
+        Customer = new CustomerDTO
+        {
+            Id = appointment.Customer.Id,
+            FirstName = appointment.Customer.FirstName,
+            LastName = appointment.Customer.LastName,
+            PhoneNumber = appointment.Customer.PhoneNumber,
+            Email = appointment.Customer.Email,
+            Appointments = null
+        },
+        Scheduled = appointment.Scheduled,
+        IsComplete = appointment.IsComplete,
+        IsCanceled = appointment.IsCanceled,
+        AppointmentServices = appointment.AppointmentServices.Select(aserv => new AppointmentServiceDTO
+        {
+            Id = aserv.Id,
+            AppointmentId = aserv.AppointmentId,
+            ServiceId = aserv.ServiceId,
+            Service = new ServiceDTO
+            {
+                Id = aserv.Service.Id,
+                Name = aserv.Service.Name,
+                Cost = aserv.Service.Cost
+            }
+        }).ToList(),
+    });
+});
+
 // 3. Endpoint to get all Customers
 // 4. Endpoint to get a Customer by Id
 // 5. Endpoint to get all Services
