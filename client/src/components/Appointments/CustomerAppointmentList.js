b8da776131b435e8590badb0e8ef465533dcb788
Josh Rittenberry <141363044+JoshRittenberry@users.noreply.github.com>
2024-01-09 09:15:29 -0600

    Added a button to view services

--- client/src/components/Appointments/CustomerAppointmentList.js
+++ client/src/components/Appointments/CustomerAppointmentList.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getCustomerById } from "../../data/customersData";
-import { Table } from "reactstrap";
+import { Button, Table } from "reactstrap";
 
 export const CustomerAppointmentList = () => {
     const params = useParams();
@@ -15,93 +15,130 @@ export const CustomerAppointmentList = () => {
         return new Date(); // Returns the current date and time
     };
 
-    function formatReadableDate(dateString) {
+    const formatReadableDate = (dateString) => {
         const options = { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' };
         const date = new Date(dateString);
         return date.toLocaleDateString('en-US', options);
     }
 
+    const hasUpcomingAppointments = () => {
+        const today = getCurrentDate();
+        return customer.appointments?.some(a => new Date(a.scheduled) >= today);
+    };
+
+    const hasPreviousAppointments = () => {
+        const today = getCurrentDate();
+        return customer.appointments?.some(a => new Date(a.scheduled) < today);
+    };
+
     return (
         <>
             <h2>Appointments</h2>
+
             {/* Upcomming Appointments */}
             <section>
-                <h3>Upcomming Appointments</h3>
-                <Table>
-                    <thead>
-                        <tr>
-                            <th>Date</th>
-                            <th>Stylist</th>
-                            <th>Status</th>
-                            <th>Cost</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {customer.appointments?.map(a => {
-                            const appointmentDate = new Date(a.scheduled);
-                            const today = getCurrentDate();
+                <h3>Upcoming Appointments</h3>
+                {hasUpcomingAppointments() ? (
+                    <Table>
+                        <thead>
+                            <tr>
+                                <th>Date</th>
+                                <th>Stylist</th>
+                                <th>Status</th>
+                                <th>Cost</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {customer.appointments?.map(a => {
+                                const appointmentDate = new Date(a.scheduled);
+                                const today = getCurrentDate();
 
-                            if (appointmentDate >= today) {
-                                return (
-                                    <tr key={a.id}>
-                                        <td>{formatReadableDate(a.scheduled)}</td>
-                                        <td>{a.stylist.firstName} {a.stylist.lastName}</td>
-                                        {a.isComplete && (
-                                            <td>Completed</td>
-                                        )}
-                                        {!a.isComplete && a.isCanceled && (
-                                            <td>Canceled</td>
-                                        )}
-                                        {!a.isComplete && !a.isCanceled && (
-                                            <td>Scheduled</td>
-                                        )}
-                                        <td>{a.totalCost}</td>
-                                    </tr>
-                                );
-                            }
-                        })}
-                    </tbody>
-                </Table>
+                                if (appointmentDate >= today) {
+                                    return (
+                                        <tr key={a.id}>
+                                            <td>{formatReadableDate(a.scheduled)}</td>
+                                            <td>{a.stylist.firstName} {a.stylist.lastName}</td>
+                                            {a.isComplete && (
+                                                <td>Completed</td>
+                                            )}
+                                            {!a.isComplete && a.isCanceled && (
+                                                <td>Canceled</td>
+                                            )}
+                                            {!a.isComplete && !a.isCanceled && (
+                                                <td>Scheduled</td>
+                                            )}
+                                            <td>{a.totalCost}</td>
+                                            <td>
+                                                <Button onClick={() => {
+                                                    let list = "";
+                                                    a.appointmentServices.forEach(aserv => {
+                                                        list += aserv.service.name + "\n"; // Adding each service name to the list with a newline character
+                                                    });
+                                                    window.alert(list); // Displaying the list in the alert
+                                                }}>Services</Button>
+                                            </td>
+                                        </tr>
+                                    );
+                                }
+                            })}
+                        </tbody>
+                    </Table>
+                ) : (
+                    <p>No Upcoming Appointments</p>
+                )}
             </section>
-
+            
             {/* Previous Appointments */}
             <section>
                 <h3>Previous Appointments</h3>
-                <Table>
-                    <thead>
-                        <tr>
-                            <th>Date</th>
-                            <th>Stylist</th>
-                            <th>Status</th>
-                            <th>Cost</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {customer.appointments?.map(a => {
-                            const appointmentDate = new Date(a.scheduled);
-                            const today = getCurrentDate();
+                {hasPreviousAppointments() ? (
+                    <Table>
+                        <thead>
+                            <tr>
+                                <th>Date</th>
+                                <th>Stylist</th>
+                                <th>Status</th>
+                                <th>Cost</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {customer.appointments?.map(a => {
+                                const appointmentDate = new Date(a.scheduled);
+                                const today = getCurrentDate();
 
-                            if (appointmentDate <= today) {
-                                return (
-                                    <tr key={a.id}>
-                                        <td>{formatReadableDate(a.scheduled)}</td>
-                                        <td>{a.stylist.firstName} {a.stylist.lastName}</td>
-                                        {a.isComplete && (
-                                            <td>Completed</td>
-                                        )}
-                                        {!a.isComplete && a.isCanceled && (
-                                            <td>Canceled</td>
-                                        )}
-                                        {!a.isComplete && !a.isCanceled && (
-                                            <td>Scheduled</td>
-                                        )}
-                                        <td>{a.totalCost}</td>
-                                    </tr>
-                                );
-                            }
-                        })}
-                    </tbody>
-                </Table>
+                                if (appointmentDate <= today) {
+                                    return (
+                                        <tr key={a.id}>
+                                            <td>{formatReadableDate(a.scheduled)}</td>
+                                            <td>{a.stylist.firstName} {a.stylist.lastName}</td>
+                                            {a.isComplete && (
+                                                <td>Completed</td>
+                                            )}
+                                            {!a.isComplete && a.isCanceled && (
+                                                <td>Canceled</td>
+                                            )}
+                                            {!a.isComplete && !a.isCanceled && (
+                                                <td>Scheduled</td>
+                                            )}
+                                            <td>{a.totalCost}</td>
+                                            <td>
+                                                <Button onClick={() => {
+                                                    let list = "";
+                                                    a.appointmentServices.forEach(aserv => {
+                                                        list += aserv.service.name + "\n"; // Adding each service name to the list with a newline character
+                                                    });
+                                                    window.alert(list); // Displaying the list in the alert
+                                                }}>Services</Button>
+                                            </td>
+                                        </tr>
+                                    );
+                                }
+                            })}
+                        </tbody>
+                    </Table>
+                ) : (
+                    <p>No Previous Appointments</p>
+                )}
             </section>
         </>
     );
